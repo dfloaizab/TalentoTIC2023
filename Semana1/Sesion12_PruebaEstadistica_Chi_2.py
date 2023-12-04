24d8ef23a9a29e46f5b27d860cd2f529af48d5da
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-12-04 18:03:06 -0500

    Update and rename Sesion12_PruebaEstadistica_Chi_2 to Sesion12_PruebaEstadistica_Chi_2.py

--- Semana1/Sesion12_PruebaEstadistica_Chi_2.py
+++ Semana1/Sesion12_PruebaEstadistica_Chi_2.py
@@ -53,10 +53,43 @@ else:
 #crear una muestra con las siguientes columnas:
 #ESTU_MCPIO_RESIDE, FAMI_TIENECOMPUTADOR, FAMI_NUMLIBROS, FAMI_SITUACIONECONOMICA
 
-data = {
-    "id" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
-    "nacionalidad" : [ "Colombiano", "Norteamericano", "Italiano",....],
-    "mano_habil": ["Diestro","Zurdo",....]
+# data = {
+#     "id" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+#     "nacionalidad" : [ "Colombiano", "Norteamericano", "Italiano",....],
+#     "mano_habil": ["Diestro","Zurdo",....]
+# }
 
-}
+#EJERCICIO: Dataset saber11.csv 
+# convertir datos de puntajes a escala categorica
+# aplicar prueba estadística chi cuadrado 
 
+#cargar el dataset
+df_icfes = pd.read_csv("Datasets/saber11.csv")
+
+#encontrar el mínimo y el máximo para la variable que queremos convertir en una categoría:
+print(df_icfes["PUNT_GLOBAL"].min())
+print(df_icfes["PUNT_GLOBAL"].max())
+
+#crear valores categóricos de acuerdo a la variable numérica en una nueva columna:
+df_icfes['Nivel_Calif'] = pd.cut(x=df_icfes['PUNT_GLOBAL'], bins=[0, 177, 277, 377, 477], 
+                     labels=['Low', 'Middle', 
+                             'High',"Higher"]) 
+
+#obtenemos la tabla de contingencia (tabla de frecuencias):
+tb_cont = pd.crosstab(df_icfes["FAMI_ESTRATOVIVIENDA"],df_icfes["Nivel_Calif"])
+print(tb_cont)
+
+#se aplica prueba estadística a la tabla de contingengia, y se obtienen los valores
+#estadísticos:
+chi2, p,a,b = chi2_contingency(tb_cont)
+print(f"Parámetro Chi 2:{chi2}")
+print(f"Parámetro p:{p}")
+
+#print(df_icfes.head())
+#
+if p < alpha:
+    # se rechaza la hipótesis nula, es decir, SÍ ha dependencia entre los datos
+    print("se rechaza la hipótesis nula, es decir, SÍ ha dependencia entre los datos")
+else:
+    # se acepta la hipótesis nula, es decir, NO hay dependencia entre los datos
+    print("se acepta la hipótesis nula, es decir, NO hay dependencia entre los datos")
